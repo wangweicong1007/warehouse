db928e61af133a2f23ea14fd2db148ea311a6a60
wangweicong1007 <33645444+wangweicong1007@users.noreply.github.com>
2019-10-12 08:48:30 +0800

    Add files via upload

---
+++ 微信测试号/usermsg.js
@@ -0,0 +1,71 @@
+﻿const titbit = require('titbit');
+const crypto = require('crypto');
+const parsexml = require('xml2js').parseString;
+const wxmsg = require('./weixinmsg');
+
+var app = new titbit();
+
+var {router} = app;
+
+//用于验证过程，在公众号验证通过后则不会再使用。
+router.get('/wx/msg', async c => {
+    var token = 'wonderful';
+
+    var urlargs = [
+        c.query.nonce,
+        c.query.timestamp,
+        token
+    ];
+
+    urlargs.sort();  //字典排序
+
+    var onestr = urlargs.join(''); //拼接成字符串
+    
+	//生成sha1签名字符串
+    var hash = crypto.createHash('sha1');
+    var sign = hash.update(onestr);
+		
+    if (c.query.signature === sign.digest('hex')) {
+        c.res.body = c.query.echostr;
+    }
+});
+
+
+
+//公众号开发者配置验证并启用后，会通过POST请求转发用户消息。
+app.router.post('/wx/msg', async c => {
+    try {
+        let data = await new Promise((rv, rj) => {
+            parsexml(c.body, {explicitArray : false}, (err, result) => {
+                if (err) {
+                    rj(err);
+                } else {
+                    rv(result.xml);
+                }
+            });
+        });
+
+        //构造需要的数据
+        let retmsg={
+            touser:data.FromUserName,
+            fromuser:data.ToUserName,
+            msgtype:'',
+            msg: data.Content,
+            msgtime:parseInt(Date.now()/1000)
+        };
+        // var data = {
+        //     touser      : xmlmsg.FromUserName,
+        //     fromuser    : xmlmsg.ToUserName,
+        //     msg         : xmlmsg.Content,
+        //     msgtime     : parseInt(Date.now() / 1000),
+        //     msgtype     : ''
+        // };
+
+        c.res.body = wxmsg.msgDispatch(data, retmsg);
+    } catch (err) {
+        console.log(err);
+    }
+
+});
+
+app.run(8003,"localhost");
